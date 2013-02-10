ea5f9e8d2e4feebd1a1319db26781c051be1ccb8
Lucas Wiener <lucas@wiener.se>
2013-02-10 22:47:30 +0100

    Refined Settings class to work with Engine

--- src/com/hyperactivity/android_app/Settings.java
+++ src/com/hyperactivity/android_app/Settings.java
@@ -2,12 +2,22 @@ package com.hyperactivity.android_app;
 
 import java.util.ArrayList;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class Settings {
+	public static final String PREF_FILE_NAME = "settingsPrefFile";
+	
+	private Context context;
 	private String id;
 	private boolean loaded;
 	private ArrayList<Color> colors;
 	
-	public Settings(String id) {
+	public Settings(Context context) {
+		this(context, "");
+	}
+	
+	public Settings(Context context, String id) {
 		this.id = id;
 		loaded = false;
 		colors = new ArrayList<Color>();
@@ -17,9 +27,63 @@ public class Settings {
 		return loaded;
 	}
 	
-	public void load() {
-		//Load data from server
+	/**
+	 * Load settings from android device.
+	 * If no settings are found the default settings gets loaded.
+	**/
+	public void loadLocal() {
+		loaded = false; //TODO: how does android handle synchronization? This will do for now.
+		
+		SharedPreferences prefs = context.getSharedPreferences(Settings.PREF_FILE_NAME, Context.MODE_PRIVATE);
+		
+		//Read all settings.
+		
+		
 		loaded = true;
+	}
+	
+	/**
+	 * Load settings from server.
+	 * If no settings are found the default settings gets loaded.
+	**/
+	public void loadRemote() {
+		loaded = false; //TODO: how does android handle synchronization? This will do for now.
+		
+		if(id.length() > 0) {
+			//Load from server
+			loaded = true;
+		}
+		else {
+			//Load default
+			loadDefault();
+		}
+	}
+	
+	/**
+	 * Load settings from server.
+	 * If no settings are found the default settings gets loaded.
+	**/
+	public void loadDefault() {
+		loaded = false; //TODO: how does android handle synchronization? This will do for now.
+		loaded = true;
+	}
+	
+	/**
+	 * Save current settings to local android device
+	 */
+	public void saveLocal() {
+		
+	}
+	
+	/**
+	 * Save current settings to user on remote server
+	 */
+	public void saveRemote() {
+		
+	}
+	
+	public void setId(String id) {
+		this.id = id;
 	}
 	
 	public Color getColor(int index) {
