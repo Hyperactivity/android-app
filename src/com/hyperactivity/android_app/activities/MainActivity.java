1a4eda91a6de3a3777f58cf01bcf4a1c499b577d
Martin Pettersson <martin.pettersson@wemind.se>
2013-04-16 15:20:08 +0200

    ThreadListFragment /Martin

--- src/com/hyperactivity/android_app/activities/MainActivity.java
+++ src/com/hyperactivity/android_app/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.hyperactivity.android_app.activities;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,9 +12,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import java.util.ArrayList;
 import android.widget.TextView;
 import com.hyperactivity.android_app.R;
 import com.hyperactivity.android_app.core.ScrollPicker;
+import com.hyperactivity.android_app.forum.ForumThread;
+import com.hyperactivity.android_app.activities.ThreadListFragment;
+
+;
 
 public class MainActivity extends FragmentActivity {
 
@@ -27,7 +33,19 @@ public class MainActivity extends FragmentActivity {
 
         View view = findViewById(R.id.forum_categories_surface_view);
         scrollPicker = (ScrollPicker) view;
-        scrollPicker.getThread().setState(ScrollPicker.ScrollPickerThread.STATE_READY);
+        scrollPicker.getThread().setState(
+                ScrollPicker.ScrollPickerThread.STATE_READY);
+
+        ArrayList<ForumThread> forumList = new ArrayList<ForumThread>();
+        forumList.add(new ForumThread(null, "test1", "test12"));
+        forumList.add(new ForumThread(null, "test2", "test22"));
+        forumList.add(new ForumThread(null, "test3", "test32"));
+
+        ThreadListFragment threadListFragment = (ThreadListFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.thread_list);
+
+        threadListFragment.updateThreadList(forumList);
+
     }
 
     @Override
@@ -44,16 +62,17 @@ public class MainActivity extends FragmentActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_settings:
-                // Settings has been clicked, check the android version to decide if to use fragmented settings or not
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-                    startActivity(new Intent(this, SettingsActivity.class));
-                } else {
-                    startActivity(new Intent(this, SettingsActivityHoneycomb.class));
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        case R.id.menu_settings:
+            // Settings has been clicked, check the android version to decide if
+            // to use fragmented settings or not
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+                startActivity(new Intent(this, SettingsActivity.class));
+            } else {
+                startActivity(new Intent(this, SettingsActivityHoneycomb.class));
+            }
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
         }
     }
 }
