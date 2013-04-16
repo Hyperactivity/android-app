e40371e84aeb160bb3b5d4687530820f72ecc9c3
Erik Odenman <erik.odenman@hotmail.com>
2013-04-16 16:28:26 +0200

    Created navigation menu fragment

--- src/com/hyperactivity/android_app/activities/NavigationMenuFragment.java
+++ src/com/hyperactivity/android_app/activities/NavigationMenuFragment.java
@@ -2,22 +2,60 @@ package com.hyperactivity.android_app.activities;
 
 import com.hyperactivity.android_app.R;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 public class NavigationMenuFragment extends Fragment {
 	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+	public static final int NUMBER_OF_BUTTONS = 5;
+	
+	private static int[] active = new int[] {R.drawable.active_house, R.drawable.active_bubble, R.drawable.active_star, R.drawable.active_search, R.drawable.active_search};
+	private static int[] inactive = new int[] {R.drawable.non_active_house, R.drawable.non_active_bubble, R.drawable.non_active_star, R.drawable.non_active_search, R.drawable.non_active_search};
+	
+	private ImageButton[] buttons;
+	private int activeButton;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.navigation_menu, container);
+		View view = inflater.inflate(R.layout.navigation_menu, container);
+		
+		buttons = new ImageButton[NUMBER_OF_BUTTONS];
+		buttons[0] = (ImageButton) view.findViewById(R.id.navigation_menu_button_1);
+		buttons[1] = (ImageButton) view.findViewById(R.id.navigation_menu_button_2);
+		buttons[2] = (ImageButton) view.findViewById(R.id.navigation_menu_button_3);
+		buttons[3] = (ImageButton) view.findViewById(R.id.navigation_menu_button_4);
+		buttons[4] = (ImageButton) view.findViewById(R.id.navigation_menu_button_5);
+		for (int i = 0; i < buttons.length; i++) {
+			final int index = i;
+			ImageButton button = buttons[i];
+			button.setBackgroundResource(inactive[i]);
+			button.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					setActiveButton(index);
+				}
+			});
+		}
+		activeButton = -1;
+		setActiveButton(0);
+		
+		return view;
+	}
+	
+	public void setActiveButton(int index) {
+		if (index != activeButton) {
+			if (activeButton != -1) {
+				buttons[activeButton].setBackgroundResource(inactive[activeButton]);
+			}
+			buttons[index].setBackgroundResource(active[index]);
+			activeButton = index;
+		}
 	}
 }
--- src/com/hyperactivity/android_app/activities/ThreadListFragment.java
+++ src/com/hyperactivity/android_app/activities/ThreadListFragment.java
@@ -3,6 +3,7 @@ package com.hyperactivity.android_app.activities;
 import java.util.*;
 
 import com.hyperactivity.android_app.R;
+import com.hyperactivity.android_app.forum.ForumThread;
 
 import android.annotation.TargetApi;
 import android.os.Bundle;
@@ -15,28 +16,31 @@ import android.widget.SimpleAdapter;
 
 @TargetApi(16)
 public class ThreadListFragment extends ListFragment {
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_2, threads);
- 
+
+	public void updateThreadList(List<ForumThread> threadList) {
 		String[] from = new String[] {"thread_headline", "thread_text"};
 		int[] to = new int[] {R.id.thread_headline, R.id.thread_text};
 		
 		List<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
-		HashMap<String, String> row1 = new HashMap<String, String>();
-		row1.put("thread_headline", "Test row 1");
-		row1.put("thread_text", "Hej! jag tänkte att liksom så atteehh...");
-		data.add(row1);
+				
+		for(int i = 0; i < threadList.size(); i++) {
+			ForumThread thread = threadList.get(i);
+			data.add(threadToMap(thread));
+		}
 		
-		HashMap<String, String> row2 = new HashMap<String, String>();
-		row2.put("thread_headline", "hejahksdjfhkj");
-		row2.put("thread_text", "Hej!fsakjl oaskopsdk fok");
-		data.add(row2);
-		
-        SimpleAdapter adapter = new SimpleAdapter(inflater.getContext(), data, R.layout.thread_list_item, from, to);
+        SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), data, R.layout.thread_list_item, from, to);
+        
         /** Setting the list adapter for the ListFragment */
         setListAdapter(adapter);
  
-        return super.onCreateView(inflater, container, savedInstanceState);
     }
+	
+	private HashMap<String, String> threadToMap(ForumThread thread) {
+		String headline = thread.getHeadline();
+		String text = thread.getText();
+		HashMap<String, String> row = new HashMap<String, String>();
+		row.put("thread_headline", headline);
+		row.put("thread_text", text);
+		return row;
+	}
 }
