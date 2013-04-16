c8fecc8aa9a2d107d8bc247c7f403997abf6621b
kbbyvonne <kbbyvonne@gmail.com>
2013-04-16 11:39:38 +0200

    added files, mainActivity

--- src/com/hyperactivity/android_app/activities/MainActivity.java
+++ src/com/hyperactivity/android_app/activities/MainActivity.java
@@ -1,14 +1,18 @@
 package com.hyperactivity.android_app.activities;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +22,7 @@ public class MainActivity extends Activity {
 
 	private Boolean	isLocked;
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,7 +34,41 @@ public class MainActivity extends Activity {
 		final View topBar = (View) findViewById(R.id.top_bar);
 		final ImageView lockImage = (ImageView) findViewById(R.id.lockButton);
 		final TextView lockTextView = (TextView) findViewById(R.id.lockText);
-
+		Display display = getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		int width;
+		int height;
+		if (android.os.Build.VERSION.SDK_INT >= 13){
+		display.getSize(size);
+		width = size.x;
+		height = size.y;
+		}else{
+			width = display.getWidth();
+			height = display.getHeight();
+		}
+		
+		width=width/5;
+		double height2=width*0.87;
+		
+		
+		
+		final ImageButton button1 = (ImageButton) findViewById(R.id.button1);
+		final ImageButton button2 = (ImageButton) findViewById(R.id.button2);
+		final ImageButton button3 = (ImageButton) findViewById(R.id.button3);
+		final ImageButton button4 = (ImageButton) findViewById(R.id.button4);
+		final ImageButton button5 = (ImageButton) findViewById(R.id.button5);
+/**
+		//button1.getLayoutParams().width=width/5;
+		button1.getLayoutParams().height=(int) height2;
+		//button2.getLayoutParams().width=width/5;
+		button2.getLayoutParams().height=(int) height2;
+		button3.getLayoutParams().width=width/5;
+		button3.getLayoutParams().height=(int) height2;
+		button4.getLayoutParams().width=width/5;
+		button4.getLayoutParams().height=(int)height2;
+		//button5.getLayoutParams().width=width/5;
+		button5.getLayoutParams().height=(int)height2;
+		**/
 		lockImage.setOnClickListener(new OnClickListener()
 		{
 			@Override
@@ -48,6 +87,9 @@ public class MainActivity extends Activity {
 				}
 			}
 		});
+		
+		
+		
 	}
 
 	@Override
